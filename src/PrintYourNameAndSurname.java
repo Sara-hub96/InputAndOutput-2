113f95e9cc4d7115feac8151ecea4ee51b8fc032
Sara <saracoppola996@gmail.com>
2023-07-12 11:35:44 +0200

    stampa nome e cognome

--- src/PrintYourNameAndSurname.java
+++ src/PrintYourNameAndSurname.java
@@ -1,6 +1,6 @@
 public class PrintYourNameAndSurname {
     public static void main(String[] args) {
-        System.out.print("Sara ");
-        System.out.println("Coppola");
+        System.out.print("Sara" + " ");
+        System.out.print("Coppola");
     }
 }
